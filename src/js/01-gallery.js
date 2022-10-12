d9f92f23f1e55fa0b86cdd8ef1a46d8ce8a6e167
Yurii Shatskyi <yurii.shatskyi.it@gmail.com>
2022-10-12 17:14:30 +0300

    first task complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createPhotoGallery(items) {
+    return items.map(item =>
+        `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join('');
+};
+
+const divPhotos = document.querySelector('.gallery');
+const addPhotoGallery = createPhotoGallery(galleryItems);
+
+divPhotos.insertAdjacentHTML("afterbegin", addPhotoGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt", 
+    captionDelay: 250,
+});
