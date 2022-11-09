b69efe8e84b0e7fe82dbde16fc6d42df4bb3af56
Yurii Shatskyi <yurii.shatskyi.it@gmail.com>
2022-11-09 11:47:42 +0200

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle'
 
+const refs = { 
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+    emailRef: document.querySelector('input'),    
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener("input", throttle(formDataInput, 500));
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    console.log({email: refs.emailRef.value, message: refs.textarea.value});
+
+    evt.currentTarget.reset()
+  
+    localStorage.clear()
+}
+
+function formDataInput(evt) {  
+    const formData = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
+  };
+  
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+    
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    
+    localStorage.setItem("feedback-form-state", message);
+    
+}
+
+function getCurrentValuesOnForm() {
+    const localStorageData = localStorage.getItem('feedback-form-state');
+        if (localStorageData) {
+    const currentData = JSON.parse(localStorageData);
+    emailRef.value = currentData.email;
+    messageRef.value = currentData.message;
+}
+};
+
+getCurrentValuesOnForm();
